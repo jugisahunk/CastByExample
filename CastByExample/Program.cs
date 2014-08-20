97b69afd9c9e92f378a16d94807321a32057e973
Jason Knight <jason.t.knight@gmail.com>
2014-08-20 07:47:55 -0500

    Added dynamic casting example between cat and dog
    The code explores the effect of casting one dynamic type into another.
    The effect can be to overwrite the property values of the cast object.
    
    I'm not sure if this still occurs if the properties are in a different
    order.

--- CastByExample/Program.cs
+++ CastByExample/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CastByExample
 {
@@ -10,6 +6,33 @@ namespace CastByExample
     {
         static void Main( string[] args )
         {
+            var cat = new
+            {
+                IsCondescending = true,
+                IsLoyalToAFault = false
+            };
+
+            var dog = new
+            {
+                IsCondescending = false,
+                IsLoyalToAFault = true
+            };
+
+            Console.WriteLine("A cat is condescending ({0}) and loyal to a fault ({1})", cat.IsCondescending, cat.IsLoyalToAFault);
+            Console.WriteLine("A dog is condescending ({0}) and loyal to a fault ({1})", dog.IsCondescending, dog.IsLoyalToAFault);
+            
+            cat = Cast( cat, dog );
+
+            Console.WriteLine( "{0}**Magic**{0}", Environment.NewLine);
+            
+            Console.WriteLine("Now, a cat is condescending ({0}) and loyal to a fault ({1})", cat.IsCondescending, cat.IsLoyalToAFault);
+            
+            Console.ReadLine();
+        }
+
+        private static T Cast<T>( T typeHolder, Object x )
+        {
+            return (T)x;
         }
     }
 }
